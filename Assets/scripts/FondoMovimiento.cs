66460e5b64db93d787bac304da85b21a6d082bb3
Javier Flores <willams_21@hotmail.com>
2018-11-19 09:45:38 -0400

    spacenave

---
+++ Assets/scripts/FondoMovimiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FondoMovimiento : MonoBehaviour {
+
+    public Renderer render;
+    public float scrolspeed = 0.1f;
+   
+    // Use this for initialization
+    void Start () {
+        render = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        render.material.mainTextureOffset = render.material.mainTextureOffset + new Vector2(1, Time.deltaTime * scrolspeed);
+    }
+
+
+}
